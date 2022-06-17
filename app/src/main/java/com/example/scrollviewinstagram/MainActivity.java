4653faa571b68cbcb154ac064ea18972b981714e
chodvadiyanandini <chodvadiyanandini@gmail.com>
2022-06-17 09:25:35 +0530

    instagram ui

---
+++ app/src/main/java/com/example/scrollviewinstagram/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.scrollviewinstagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        Log.e("TAG", "onStart: ===> "+"onstart" );
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        Log.e("TAG", "onResume: ===> "+"onResume" );
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        Log.e("TAG", "onPause: ===> "+"onPause" );
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        Log.e("TAG", "onStop: ===> "+"onStop" );
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        Log.e("TAG", "onDestroy: ===> "+"onDestroy" );
+//    }
+}
